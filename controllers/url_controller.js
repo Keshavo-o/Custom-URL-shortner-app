2f461f0b0ae86ffa5e4f6e512beba7ce2d610573
Keshav Verma <148549557+Keshavo-o@users.noreply.github.com>
2025-08-15 15:35:32 +0530

    Update url_controller.js

--- controllers/url_controller.js
+++ controllers/url_controller.js
@@ -4,16 +4,18 @@ async function createurl(req,res)
 {
     const body = req.body;
     const redirected = body.url;//provided by user
+    const all_urls = await url.find({});
+    console.log(redirected);
     if(!redirected)
     {
-        return res.status(400).json({error: "URL is required"});
+        return res.status(400).json({error: "URL is required,bhai"});
     }
     const shortid = nanoid(8);//generates a random 8-character string
     await url.create({ 
         short_id: shortid,
         redirect_url: redirected
     });
-    return res.status(201).json({message: "URL created successfully", shortid});
+return res.render("url_created", { recent_id: shortid,});
 }
 async function geturl(req,res)
 {
@@ -23,16 +25,15 @@ async function geturl(req,res)
 }
 async function handlegeturlbyid(req,res)
 {
-    const redirected_id = req.params.url;
-    const url_data = await url.findOne({redirect_url: redirected_id});
+    const short_id = req.params.url;
+    const url_data = await url.findOne({short_id: short_id});
     if(!url_data)
     {
-        return res.status(404).json({error: "URL not found"});
+        return res.status(404).json({error: "URL not not found"});
     }
     const url_id = url_data._id;
     await url.findByIdAndUpdate(url_id, { click_count: url_data.click_count+1 } );
-    return res.json({message: "URL fetched successfully", short_id: url_data.short_id, click_count: url_data.click_count});
-    //if you want to redirect here instead of returning a response as json -  you can use res.redirect(url_data.redirect_id)
+    return res.redirect(url_data.redirect_url);
 }
 async function giveanalytics(req,res)
 {
@@ -49,5 +50,5 @@ module.exports = {
     geturl,
     handlegeturlbyid,
     giveanalytics
-
 }
+
